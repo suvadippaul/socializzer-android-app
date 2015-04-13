bb1817c8055ab871209caa2e05d8b11e88940934
Suvadip Paul <titashpaul@gmail.com>
2015-04-13 18:25:37 +0530

    Added fb user i/o

---
+++ socializzer/app/src/main/java/com/example/suvadip/socializzer/LoginBroadcastReceiver.java
@@ -0,0 +1,30 @@
+package com.example.suvadip.socializzer;
+
+import android.os.Bundle;
+import android.util.Log;
+import com.facebook.FacebookBroadcastReceiver;
+
+/**
+ * Created by suvadip on 12/4/15.
+ */
+
+
+/**
+ * This is a simple example to demonstrate how an app could extend FacebookBroadcastReceiver to handle
+ * notifications that long-running operations such as photo uploads have finished.
+ */
+public class LoginBroadcastReceiver extends FacebookBroadcastReceiver {
+
+    @Override
+    protected void onSuccessfulAppCall(String appCallId, String action, Bundle extras) {
+        // A real app could update UI or notify the user that their photo was uploaded.
+        Log.d("HelloFacebook", String.format("Photo uploaded by call " + appCallId + " succeeded."));
+    }
+
+    @Override
+    protected void onFailedAppCall(String appCallId, String action, Bundle extras) {
+        // A real app could update UI or notify the user that their photo was not uploaded.
+        Log.d("HelloFacebook", String.format("Photo uploaded by call " + appCallId + " failed."));
+    }
+}
+
